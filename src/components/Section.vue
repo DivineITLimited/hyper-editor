43bcbbb9fdf168cdd2ac9583a8c9a62568a96a1c
Shimul Chowdhury <shimul@divine-it.net>
2019-05-05 00:48:51 +0600

    Section moved to tailwind

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,83 +1,64 @@
 <template>
-    <section :class="'hyp-section ' + hoverClass" @mouseover="onMouseEnter" @mouseout="onMouseLeave">
+  <section :class="'hyp-section ' + hoverClass" @mouseover="onMouseEnter" @mouseout="onMouseLeave">
 
-        <ActionBar
-            class="action-bar"
-            name="Section"
-            :id="id"
-            moveBtnClass="smHandle"
-            :collapseAction="onCollapse"
-            :collapsed="collapsed"
-            :cloneAction="clone"
-            :editAction="edit"
-            :removeAction="remove" />
+    <ActionBar
+      class="action-bar"
+      name="Section"
+      :id="id"
+      moveBtnClass="smHandle"
+      :collapseAction="onCollapse"
+      :collapsed="collapsed"
+      :cloneAction="clone"
+      :editAction="edit"
+      :removeAction="remove"/>
+    <Nester :class="'hyp-container' + (getSelf.settings.type == 'Fluid' ? '-fluid' : '')" v-if="!collapsed"
+            :id="this.id"/>
 
-        <!-- <draggable v-if="!collapsed" v-model="children" class="vc-container" :options="{handle:'.rmHandle', animation:150}">
-            <Row v-for="child in children" :key="child.id" handle='rmHandle' :id="child.id" />
-        </draggable> -->
-
-            <Nester :class="'hyper-container' + (getSelf.settings.type == 'Fluid' ? '-fluid' : '')" v-if="!collapsed" :id="this.id" />
-
-
-    </section>
+  </section>
 </template>
 
 <script>
 
-import Row from './Row'
-import draggable from 'vuedraggable'
-import { ADD_COMPONENT, REARRANGE_EDITOR, REMOVE_COMPONENT } from '../mutation-types'
-import { cloneable, removeable, editable, collapseable, hasHoverControl } from '../mixins/base'
+  import Row from './Row'
+  import draggable from 'vuedraggable'
+  import {ADD_COMPONENT, REARRANGE_EDITOR, REMOVE_COMPONENT} from '../mutation-types'
+  import {cloneable, removeable, editable, collapseable, hasHoverControl} from '../mixins/base'
 
-export default {
-    name:'eSection',
+  export default {
+    name: 'eSection',
     props: ['id'],
     mixins: [cloneable, removeable, editable, collapseable, hasHoverControl],
-    components: { Row, draggable },
+    components: {Row, draggable},
     data() {
-        return {
-            collapsed: false
-        }
+      return {
+        collapsed: false
+      }
     },
     computed: {
-        children: {
-            get() {
-                return this.$store.getters.findComponentByID(this.id).children
-            },
-            set(value) {
-                this.$store.commit(REARRANGE_EDITOR, { value, id:this.id, getters: this.$store.getters })
-            }
+      children: {
+        get() {
+          return this.$store.getters.findComponentByID(this.id).children
         },
-
-        getSelf () {
-            return this.$store.getters.findComponentByID(this.id)
+        set(value) {
+          this.$store.commit(REARRANGE_EDITOR, {value, id: this.id, getters: this.$store.getters})
         }
+      },
+
+      getSelf() {
+        return this.$store.getters.findComponentByID(this.id)
+      }
     }
-}
+  }
 </script>
 
 <style lang="scss">
-.hyp-section {
-    &:first-child{
-      margin-top:45px;
-    }
-    margin: 0 1%;
+  .hyp-section {
+    @apply block relative border border-dashed border-grey-light mx-2 bg-grey-lighter;
     min-height: 20px;
-    display: block !important;
-    position: relative;
-    background: #f5f5f5;
-    border: 1px dashed rgba(170,170,170,0.9);
-    .vc-container {
-        width: 85%;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 0;
-        margin-bottom: 20px;
-    }
-    .hyper-container{
-      width: 85%;
-      margin: 5px auto;
-    }
-}
+  }
+
+  .hyp-container {
+    @apply mx-2 my-1;
+  }
 
 </style>
