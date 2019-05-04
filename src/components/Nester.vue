319144e692aeeb33db205eca8d68c8a862cd3802
Shimul Chowdhury <shimul@divine-it.net>
2019-05-05 00:39:41 +0600

    Nester style

--- src/components/Nester.vue
+++ src/components/Nester.vue
@@ -1,82 +1,63 @@
 <template>
-    <draggable v-model="children" v-bind="{handle:'.blHandle', animation:150, group:'moveable'}">
+  <draggable v-model="children" v-bind="{handle:'.blHandle', animation:150, group:'moveable'}">
 
-        <ComponentLoader v-for="child in children" :key="child.id" :name="child.type" :id="child.id" handle='blHandle' />
-
-        <div slot="footer">
-            <p  v-if="children.length <= 0" class="noBlockMessage">Add A Block Here</p>
-            <div class="addBlocksButtonWrapper">
-                <b-btn @click="chooseBlock" variant="addBlocksButton" title="Add Block">
-                    <icon name="plus"></icon>
-                </b-btn>
-            </div>
-        </div>
-    </draggable>
+    <ComponentLoader v-for="child in children" :key="child.id" :name="child.type" :id="child.id" handle='blHandle'/>
+    <div>
+      <p v-if="children.length <= 0" class="hyp-nester-desc">Add A Block Here</p>
+      <div class="hyp-nester-btn-wrap">
+        <button type="button" @click="chooseBlock" class="hyp-nester-add-btn" title="Add Block">
+          <icon name="plus" stroke="3"></icon>
+        </button>
+      </div>
+    </div>
+  </draggable>
 </template>
 
 <script>
-import draggable from 'vuedraggable'
-import { removeable, cloneable, editable, childsortable } from '../mixins/base'
-import { SHOW_BLOCK_CHOOSER_FOR, REARRANGE_EDITOR } from '../mutation-types'
-import Block from './Block'
+  import draggable from 'vuedraggable'
+  import {removeable, cloneable, editable, childsortable} from '../mixins/base'
+  import {SHOW_BLOCK_CHOOSER_FOR, REARRANGE_EDITOR} from '../mutation-types'
+  import Block from './Block'
 
-export default {
+  export default {
     props: ['id'],
     mixins: [childsortable],
-    components: { Block, draggable },
-    created () {
-        // console.log('Nester with id ' + this.id)
+    components: {Block, draggable},
+    created() {
+      // console.log('Nester with id ' + this.id)
     },
     computed: {
-        getSelf () {
-            return this.$store.getters.findComponentByID(this.id)
-        }
+      getSelf() {
+        return this.$store.getters.findComponentByID(this.id)
+      }
     },
     methods: {
-        chooseBlock() {
-            this.$store.commit(SHOW_BLOCK_CHOOSER_FOR, this.id)
-        }
+      chooseBlock() {
+        this.$store.commit(SHOW_BLOCK_CHOOSER_FOR, this.id)
+      }
     }
-};
+  };
 
 
 </script>
 <style lang="scss">
-
-
-  .addBlocksButtonWrapper{
-      text-align: center;
-      margin-bottom: 8px;
-    b-btn {
-      -webkit-box-shadow: rgba(0,0,0,0.12) 0 1px 4px -1px;
-      box-shadow: rgba(0,0,0,0.12) 0 1px 4px -1px;
-      background-color: #fff;
-      border-radius: 3px;
-      overflow: hidden;
-      margin: 0;
-      padding: 5px 8px;
-      display: inline-flex;
-      i {
-        display: inline-grid;
-        vertical-align: middle;
-        svg {
-          width: 13px;
-          height: 13px;
-        }
-      }
-      &:hover {
-        background: #007bff;
-        color: #fff;
-        cursor: pointer;
-      }
-    }
-
+  .hyp-nester-desc {
+    @apply text-xs text-grey text-center;
   }
-  p.noBlockMessage {
-        font-size: 11px;
-        font-style: italic;
-        margin-bottom: 2px;
-        text-align: center;
-        color: #e1e0e0;
-      }
+
+  .hyp-nester-btn-wrap {
+    @apply text-center mb-2;
+  }
+
+  .hyp-nester-add-btn {
+    @apply px-2 rounded bg-white shadow text-grey-dark mt-1;
+    padding-top: .2rem;
+    padding-bottom: .2rem;
+    &:focus {
+      @apply outline-none;
+    }
+    &:hover {
+      @apply bg-blue text-white;
+    }
+  }
 </style>
